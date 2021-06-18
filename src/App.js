856b56a6adff4403b3f64e18ff9e234eddf815b8
Akshay S <akshayshan28@gmail.com>
2021-06-19 00:18:16 +0530

    fixed the issue

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { Apicall , ipwhois} from './apicall/Apicall';
-import Base from './base/Base';
-import Layout from './base/layout';
+import React, { useState, useEffect } from "react";
+import { Apicall, ipwhois } from "./apicall/Apicall";
+import Base from "./base/Base";
+import Layout from "./base/layout";
 
+const App = () => {
+  const [ipdetails, setIpdetails] = useState([]);
+  const [whois, setWhois] = useState([]);
 
-const App = () =>{
-
-  const [ipdetails, setIpdetails] = useState([])
-  const [whois, setWhois] = useState([])
-
-  const getDetails = () =>{
+  const getDetails = () => {
     //e.preventDefault();
-    Apicall().then(response =>{
-      setIpdetails(response)     
-     // console.log(ipdetails)
-    })
-    ipwhois(ipdetails.IPv4).then(response =>{
-      setWhois(response)
-      console.log(whois)
-    })
-   }
-  return(
-    <div> 
-      <Layout onClick={getDetails()}/>
-      <Base ipdetails={whois}/>
-     </div>
-  )
-}
+    Apicall().then((response) => {
+      setIpdetails(response);
+      // console.log(ipdetails)
+    });
+    ipwhois(ipdetails.IPv4).then((response) => {
+      setWhois(response);
+      console.log(whois);
+    });
+  };
+  return (
+    <div>
+      <Layout onClick={getDetails} />
+      <Base ipdetails={whois} />
+    </div>
+  );
+};
 
 export default App;
