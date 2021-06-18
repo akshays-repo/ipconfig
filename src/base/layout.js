e5809beea9cf7fb31746a73ff277285b9b9eb595
Akshay S <akshayshan28@gmail.com>
2021-06-19 00:27:47 +0530

    removed content

--- src/base/layout.js
+++ src/base/layout.js
@@ -7,27 +7,6 @@ const Layout = ({ getDetails }) => {
       <header className="header header--purple">
         <h1 className="header__title font-display">ALL ABOUT IP</h1>
       </header>
-      <div className="info">
-        <p className="info__text font-body">
-          This is where I share all of my great creative work! Here are some
-          links that will direct you toward my awesome channels.
-        </p>
-        <ul className="info__link-list">
-          <li className="info__link-list-item">
-            <a className="button font-display" href="#">
-              link 1
-            </a>
-          </li>
-          <li className="info__link-list-item">
-            <button
-              className="button font-display"
-              onClick={() => getDetails()}
-            >
-              Get Details
-            </button>
-          </li>
-        </ul>
-      </div>
     </div>
   );
 };
